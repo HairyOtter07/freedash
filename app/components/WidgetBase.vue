3bf8086f51f12870c20ed6839f9185c27e14cf70
HairyOtter07 <hairyotter07@gmail.com>
2026-08-13 21:48:58 -0700

    begin implementing dragging

--- app/components/WidgetBase.vue
+++ app/components/WidgetBase.vue
@@ -1,7 +1,52 @@
 <template>
   <div
-    class="flex h-full w-full flex-col items-center justify-center border border-zinc-100 bg-zinc-800"
+    ref="widget"
+    class="group relative flex h-full w-full flex-col items-center justify-center rounded-xl border-zinc-100 bg-zinc-700"
+    :class="isDragging ? `z-50 cursor-grabbing shadow-xl` : ``"
   >
+    <Icon
+      icon="material-symbols:drag-pan-rounded"
+      class="absolute top-0 left-0 h-5 w-5 -translate-1/2 rounded-full bg-white text-black"
+      :class="
+        isDragging
+          ? `block cursor-grabbing`
+          : `hidden group-hover:block hover:cursor-grab`
+      "
+      @mousedown="onMouseDown"
+    />
     <p class="text-white">hello</p>
   </div>
 </template>
+<script setup>
+import { Icon } from "@iconify/vue";
+
+const widget = ref(null);
+const isDragging = ref(false);
+const dragStartX = ref(0);
+const dragStartY = ref(0);
+
+const onMouseDown = (event) => {
+  event.preventDefault();
+
+  dragStartX.value = event.pageX;
+  dragStartY.value = event.pageY;
+  isDragging.value = true;
+
+  document.addEventListener("mousemove", onMouseMove);
+  document.addEventListener("mouseup", onMouseUp);
+};
+
+const onMouseMove = (event) => {
+  const offsetX = event.pageX - dragStartX.value;
+  const offsetY = event.pageY - dragStartY.value;
+  widget.value.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+};
+
+const onMouseUp = (event) => {
+  isDragging.value = false;
+  widget.value.style.transform = "none";
+
+  document.removeEventListener("mousemove", onMouseMove);
+  document.removeEventListener("mouseup", onMouseUp);
+};
+</script>
